13c239b8b91169dee2ea5ee52012293195edd8fb
JhonPoma <jpm11797@gmail.com>
2025-09-16 17:24:57 -0500

    iniciando apibus

---
+++ api-bus/src/test/java/com/example/api_bus/ApiBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
